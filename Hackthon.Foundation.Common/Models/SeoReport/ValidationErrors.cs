11f5aa0b9cae160b5ce70f8d1d345747c8366166
preddysi <preddysi@IP-AC1F5BF0>
2019-03-02 10:09:09 -0800

    added proj with errors

---
+++ Hackthon.Foundation.Common/Models/SeoReport/ValidationErrors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hackthon.Foundation.Common.Models.SeoReport
+{
+    public class ValidationErrors
+    {
+        public ValidationErrors(string section, SEOLevel level, string message)
+        {
+            Section = section;
+            Level = level;
+            Message = message;
+        }
+        public string Section { get; set; }
+        public string Message { get; set; }
+        public SEOLevel Level { get; set; }
+    }
+
+    public enum SEOLevel
+    {
+        Good,
+        Warning,
+        NeedFix
+    }
+}
